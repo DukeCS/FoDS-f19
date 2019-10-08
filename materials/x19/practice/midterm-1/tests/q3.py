d57fdc3b647cf40231de776172551e5daf81d7e3
Nate Guerin <nate@textpla.in>
2019-10-08 17:49:39 -0400

    practice midterm with answers

---
+++ materials/x19/practice/midterm-1/tests/q3.py
@@ -0,0 +1,33 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # The table should have 17 rows
+          >>> clippers_players.num_rows
+          17
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> # The table should be the same as the one in the csv
+          >>> x = Table().read_table('tests/q3_result.csv')
+          >>> tables_are_same(x, q3_ans)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
